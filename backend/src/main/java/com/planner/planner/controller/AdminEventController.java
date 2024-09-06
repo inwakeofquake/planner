122791489913b6bfb49cb36f97a679e96938403d
Андрей Кузьмин <ku7min@yandex.ru>
2024-09-06 14:51:54 +0300

    Initial project setup with backend and frontend

---
+++ backend/src/main/java/com/planner/planner/controller/AdminEventController.java
@@ -0,0 +1,8 @@
+package com.planner.planner.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminEventController {
+
+}
---
+++ backend/src/main/java/com/planner/planner/enums/Coverage.java
@@ -0,0 +1,12 @@
+package com.planner.planner.enums;
+
+public enum Coverage {
+    INTERVIEW,
+    SOCIAL_MEDIA,
+    PHOTO_SHOOT,
+    VIDEO,
+    FILM,
+    LOCAL_NEWS,
+    MAIN_WEBSITE_NEWS,
+    THIRD_PARTY_COVERAGE
+}
